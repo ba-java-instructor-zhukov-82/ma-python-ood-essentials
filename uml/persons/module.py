b6e3bde20f68382d2cf5c4ba932942da2d96a3db
ba-java-instructor-zhukov-82 <ba.java.instructor.zhukov.82@gmail.com>
2017-02-02 20:58:23 +0200

    PlantUML diagram implementation fix

--- uml/persons/module.py
+++ uml/persons/module.py
@@ -14,10 +14,37 @@ class Person(Eater):
         print('Person is eating...')
 
 
+class Health:
+    SICK = 'sick'
+    EMPLOYABLE = 'employable'
+    HORSE = 'horse'
+
+    def __init__(self, health_level):
+        self.health_level = health_level
+
+    def __str__(self):
+        return '\thealth: {}\n'.format(self.health_level)
+
+
+class Tools:
+
+    def __init__(self, *tools_list):
+        self.tools_list = tools_list
+
+    def __str__(self):
+        return '\ttools: {}\n'.format(self.tools_list)
+
+
 class Worker(Eater):
+    def __init__(self, health, tools):
+        self._health = health
+        self.tools = tools
 
     def eat(self):
         print('Worker is eating...')
+
+    def __str__(self):
+        return 'Worker:\n {} {}'.format(self._health.__str__(), self.tools.__str__())
 
 
 def do_eat(eaters):
@@ -35,6 +62,22 @@ except Exception:
     print('Interface type can not be instantiated!')
 
 
-person = Person()
-worker = Worker()
-do_eat([person, worker])
+def eat_test():
+    person = Person()
+    worker = Worker()
+    do_eat([person, worker])
+
+# eat_test()
+
+
+def composition_test():
+    worker_horse = Worker(Health(Health.HORSE))
+    print(worker_horse)
+
+# composition_test()
+
+def aggregation_test():
+    worker_equipped = Worker(Health(Health.EMPLOYABLE), Tools('nails', 'hummer'))
+    print(worker_equipped)
+
+aggregation_test()

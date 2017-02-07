8045abc041087ff616bdc19c2778a2e690f16f68
ba-java-instructor-zhukov-82 <ba.java.instructor.zhukov.82@gmail.com>
2017-02-07 21:21:24 +0200

    Proxy pattern added

--- patterns/proxy_pattern/proxy_test.py
+++ patterns/proxy_pattern/proxy_test.py
@@ -1,15 +1,35 @@
 from patterns.proxy_pattern.topic_access_proxy import Forum
+from patterns.proxy_pattern.topic_access_proxy import UserAccess
+from patterns.proxy_pattern.topic_access_proxy import User
+from patterns.proxy_pattern.topic_access_proxy import ForumProxy
 
 
 def forum_test_true():
-    forum = Forum('admin')
-    forum.load_topic('admin')
+    forum = Forum(UserAccess.ADMIN, 'admin')
+    forum.load_topic(UserAccess.ADMIN, 'admin')
 
 
 def forum_test_false():
-    forum = Forum('admin')
-    forum.load_topic('secret')
+    forum = Forum(UserAccess.ADMIN, 'admin')
+    forum.load_topic(UserAccess.ADMIN, 'secret')
 
 
-forum_test_true()
-forum_test_false()
+def proxy_forum_test_access_true():
+    admin = User(UserAccess.ADMIN)
+    ForumProxy.load_topic(admin, 'admin')
+
+
+def proxy_forum_test_access_false():
+    guest = User(UserAccess.GUEST)
+    ForumProxy.load_topic(guest, 'admin')
+
+def proxy_forum_test_topic_name_false():
+    guest = User(UserAccess.GUEST)
+    ForumProxy.load_topic(guest, 'secret')
+
+# forum_test_true()
+# forum_test_false()
+
+proxy_forum_test_access_true()
+proxy_forum_test_access_false()
+proxy_forum_test_topic_name_false()
--- patterns/proxy_pattern/topic_access_proxy.py
+++ patterns/proxy_pattern/topic_access_proxy.py
@@ -1,13 +1,43 @@
 class Forum:
 
-    def __init__(self, *topic_name):
-        self.__topic_name = topic_name
+    def get_topics(self):
+        return self.__topics
+
+    def __init__(self, topic_access, topic_name):
+        self.__topics = dict()
+        self.__topics[topic_name] = topic_access
 
     def load_topic(self, topic_name):
-        if topic_name in self.__topic_name:
+        if topic_name in self.__topics:
             print('requested topic {} loaded successfully!'.format(topic_name))
         else:
             print('there is no topic with requested name!')
 
 
+class UserAccess:
 
+    GUEST = 'GUEST'
+    USER = 'USER'
+    ADMIN = 'ADMIN'
+
+
+class User:
+
+    def __init__(self, access):
+        self.access = access
+
+
+class ForumProxy:
+
+    @staticmethod
+    def load_topic(user: User, topic_name):
+        forum = Forum(UserAccess.ADMIN, 'admin')
+        topics = forum.get_topics()
+        if topic_name in topics:
+            if user.access == topics[topic_name]:
+                forum.load_topic(topic_name)
+            else:
+                print('access denied! you have no required access! Yours is {}'
+                      .format(user.access))
+        else:
+            print('there is no such topic on forum!')
